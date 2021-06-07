91aaf049c608286bf24b7c95d3071202e81fa916
lihbr <lihbr@users.noreply.github.com>
2021-06-07 12:41:20 +0200

    feat: export I18nManager and CreateInfiniteDicoData

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./createDico";
-export * from "./fetchDicoInternalHandler";
 export * from "./DicoError";
+export * from "./I18nManager";
 
-export type { DicoDataJSON } from "./types";
+export type { DicoDataJSON, CreateInfiniteDicoData } from "./types";
